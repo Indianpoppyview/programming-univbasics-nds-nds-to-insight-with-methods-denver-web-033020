4b78adca27564fbecd9cc26dfd32c862309a634e
David Montoya <davidcmontoya@yahoo.com>
2020-03-19 16:59:24 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -19,10 +19,11 @@ end
 # { directorOne => allTheMoneyTheyMade, ... }
 def directors_totals(nds)
   result = {}
-  director_index = 0 
-  while director_index < nds.size do 
+  director_index = 0
+  while director_index < nds.size do
     director = nds[director_index]
-    director_index += 1 
+    result[director[:name]] = gross_for_director(director)
+    director_index += 1
+  end
+  result
 end
-result
-end 
